b40e99161b5a58e6402f1afea0bf79a4e74145b8
ryannolanco <ryannolanco@gmail.com>
2025-01-17 01:51:54 -0700

    update: started creating toggle for nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -3,34 +3,43 @@ import './navigation.css';
 
 const Navigation = () => {
 	return (
-		<nav className="navigation">
-			<ul className="navigation__container">
-				<li>
-					<a href="" className="linkedin-link">
-						<span className="nav-span" aria-hidden="true">
-							00
-						</span>
-						LINKEDIN
-					</a>
-				</li>
-				<li>
-					<a href="" className="email-link">
-						<span className="nav-span" aria-hidden="true">
-							01
-						</span>
-						GITHUB
-					</a>
-				</li>
-				<li>
-					<a href="" className="resume">
-						<span className="nav-span" aria-hidden="true">
-							02
-						</span>
-						RESUME
-					</a>
-				</li>
-			</ul>
-		</nav>
+		<>
+			<button
+				className="mobile-nav-toggle"
+				aria-controls="primary-navigation"
+				aria-expanded="false"
+			>
+				<span className="sr-only">Menu</span>
+			</button>
+			<nav className="navigation">
+				<ul id="primary-navigation" className="navigation__container">
+					<li>
+						<a href="" className="linkedin-link">
+							<span className="nav-span" aria-hidden="true">
+								00
+							</span>
+							LINKEDIN
+						</a>
+					</li>
+					<li>
+						<a href="" className="email-link">
+							<span className="nav-span" aria-hidden="true">
+								01
+							</span>
+							GITHUB
+						</a>
+					</li>
+					<li>
+						<a href="" className="resume">
+							<span className="nav-span" aria-hidden="true">
+								02
+							</span>
+							RESUME
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</>
 	);
 };
 
