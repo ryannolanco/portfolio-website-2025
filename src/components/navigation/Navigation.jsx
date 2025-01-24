59075e199a912834616960cbe89b1be7fcb90c6d
ryannolanco <ryannolanco@gmail.com>
2025-01-24 05:03:48 -0700

    update: mobile nav complete

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './navigation.css';
 
 const Navigation = () => {
+	const navRef = useRef(null); // Reference for the <ul> element
+	const navToggleRef = useRef(null); // Reference for the toggle button
+
+	const handleToggle = () => {
+		const nav = navRef.current;
+		const navToggle = navToggleRef.current;
+
+		if (nav && navToggle) {
+			const visibility = nav.getAttribute('data-visible');
+			if (visibility === 'false') {
+				nav.setAttribute('data-visible', true);
+				navToggle.setAttribute('aria-expanded', true);
+			} else {
+				nav.setAttribute('data-visible', false);
+				navToggle.setAttribute('aria-expanded', false);
+			}
+			console.log(visibility);
+		}
+	};
+
 	return (
 		<>
 			<button
+				ref={navToggleRef}
 				className="mobile-nav-toggle"
 				aria-controls="primary-navigation"
 				aria-expanded="false"
-				onClick={() => {
-					const nav = document.getElementById('primary-navigation'); // Access the <ul> element
-					const visibility = nav.getAttribute('data-visible');
-
-					if (visibility === 'false') {
-						nav.setAttribute('data-visible', 'true');
-					} else {
-						nav.setAttribute('data-visible', 'false');
-					}
-					console.log(visibility);
-				}}
+				onClick={handleToggle}
 			>
 				<span className="sr-only">Menu</span>
 			</button>
 			<nav className="navigation">
 				<ul
+					ref={navRef}
 					data-visible="false"
 					id="primary-navigation"
 					className="primary-navigation"
